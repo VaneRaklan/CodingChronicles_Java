a231e8767086ccd6df35caf81a5cdf5d29e1887f
VaneRaklan <anton.manuel.ramirez@gmail.com>
2017-06-07 15:37:23 -0700

    Adding in a basic java class with basic code

---
+++ Practice.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+
+class Practice{
+
+public static Scanner user = new Scanner(System.in);
+
+	public static void main(String args[]){
+		//countUp();
+		//countDown();
+		//userInput();
+		//ifLoop();
+		//whileLoop();
+		//switchStatement();
+	}
+/* //Basic code to count upwards from a lower extremity to an upper
+
+	public static void countUp(){
+		for(int i = 0; i < 10; i ++){
+			System.out.println(i)
+		}
+	}
+*/
+
+/* //Basic code to count downwards from an upper extremity to a lower
+
+	public static void countDown(){
+		for(int i = 10; i > 0; i --){
+			System.out.println(i)
+		}
+	}
+*/
+
+/* //Basic user input code to print an input
+
+	public static void userInput(){
+		Scanner user = new Scanner(System.in);
+		System.out.println(user.next());
+	}
+*/
+
+/* //Basic if statement w/ user input
+
+	public static void ifLoop(){
+		int x = user.nextInt();
+
+		if(x > 0){
+			System.out.println("Hella");
+		}
+		else if(x == 0){
+			System.out.println("Y u be zero?");
+		}
+		else if(x < 0){
+			System.out.println("Why less than zero fam?");
+		}
+	}
+*/
+
+/* //Basic while loop w/ user input
+
+	public static void whileLoop(){
+		int x = user.nextInt();
+		
+		while(x < 25){
+			System.out.println(x);
+			x ++;
+		}
+		System.out.println("Aaaaaand, we're done!");
+	}
+*/
+
+/* //Basic switch statement w/ user input
+
+	public static void switchStatement(){
+		int x = user.nextInt();
+
+		switch(x){
+			case 0:
+				System.out.println("It's zero for nothing");
+				break;
+			case -1:
+				System.out.println("Negative captain");
+				break;
+			case 1:
+				System.out.println("Are you positive about this?");
+				break;
+			default:
+				System.out.println("What the hell did you do?");
+				break;
+		}
+	}
+*/
+}
