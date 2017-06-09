0c6e9459751baf76b32a93d6f4361a5795baf760
VaneRaklan <anton.manuel.ramirez@gmail.com>
2017-06-08 21:26:26 -0700

    Added code for continue syntax and labeled continue statement

--- JavaIntermediate.java
+++ JavaIntermediate.java
@@ -3,7 +3,36 @@ class JavaIntermediate{
 	public static void main(String[] args){
 		//ternaryOp(0);
 		//labeledBreak();
+		//labeledContinue();
 	}
+/* //continue syntax and labeled continue statement
+	public static void labeledContinue(){
+		int array1[] = {0,1,2,3,4,5,6,7,8,9};
+		int array2[] = {0,1,2,3,4,5,6,7,8,9};
+		
+		for(int i : array1){
+			if(i % 2 == 0){
+				System.out.println("Skip the evens");
+				continue;
+			}
+			System.out.println(i);
+			System.out.println("It is Odd!");
+		}
+
+		outer:
+		for(int i : array1){
+			System.out.println(i);
+			inner:
+			for(int f : array2){
+				if(f % 2 == 0){
+					continue inner;
+				}
+				System.out.println(f);
+			}
+			
+		}	
+	}
+*/
 
 /* //Labeled Break
 	public static void labeledBreak(){
