b9008c881764ca0e6e6b52f695e6d8ef55f1d3c2
Antonio Ramirez <vaneraklan@D-10-157-23-164.dhcp4.washington.edu>
2017-09-02 10:46:57 -0700

    Starting Abstract Modifiers

--- JavaVol3.java
+++ JavaVol3.java
@@ -3,7 +3,22 @@ public class JavaVol3 {
 
 	public static void main(String[] args) {
 		
-		
-		
 	}
+
+
+/* //Abstract Modifiers Vol. 1
+When multiple sub classes access super class constructors but require different code for specific constructors, but not all
+we can abstract the super class and the specific methods that we need variation for.
+
+This level of abstractions can be compared to inheritence and interfaces.
+They act as an organizational tool; a guaruntee if-you-will. When you abstract a method
+in a (abstract) class, any subclasses are required to have the corresponding method
+
+Things to know/remember:
+- If at least one method in the class is abstract, then the entire class must be labeled as abstract
+- An abstract class cannot be instantiated (made into an object)
+- An abstact class can still have static variables and static methods
+
+*/
+
 }
